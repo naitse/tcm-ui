f982019907989da85564a29944b631f44dcdf589
adcarabajal <adcarabajal@gmail.com>
2013-07-05 18:58:27 -0300

    top menu plugin display

--- js/views/topmenu.js
+++ js/views/topmenu.js
@@ -18,8 +18,33 @@ define(function(require){
             if(!this.hotLink){
               this.renderBar();
             }
-            
+
+            this.renderPluginsMenu();
+
             this.attachEvents();
+
+        },
+
+        renderPluginsMenu: function(){
+
+            tcmModel.plugins.fetch().done(function(data){
+
+                _.each(data, function(p){
+                    switch( p.name ){
+                        case "Jira":
+
+                            if(p["properties"]["enabled"].indexOf("true") > -1){
+                                $("#link-sync").show();
+                            }
+                            break;
+                        case "Import_Export":
+                            if(p["properties"]["enabled"].indexOf("true") > -1){
+                                $("#link-export-import").show();
+                            }
+                            break;
+                    }
+                });
+            });
 
         },
 
