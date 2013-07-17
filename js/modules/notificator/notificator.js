bd9fc47c069998630203ea92e111d755db69bae0
adcarabajal <adcarabajal@gmail.com>
2013-07-17 16:49:07 -0300

    new endpoint

--- js/modules/notificator/notificator.js
+++ js/modules/notificator/notificator.js
@@ -7,7 +7,7 @@ define(function(require){
     var Notificator = barman.Class.create({
        channel: "default-channel",
        //socketHost: "ws://localhost:8089/ws/notificator/",
-       socketHost: "ws://50.16.102.175:8081/ws/notificator",
+       socketHost: "ws://54.226.76.211:8081/ws/notificator",
        identifier: Math.floor(Math.random()*10000),
        socket: null,
        debug: false,
